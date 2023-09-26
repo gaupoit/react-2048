8cdc67e5273197c4a77aef5e67dddc5ce40a3ed7
Thinh Nguyen <gaupoit@outlook.com>
2023-09-26 15:41:44 +0700

    Check public key

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  CasperProvider,
+  createClient,
+  CasperDashConnector,
+  CasperSignerConnector,
+  useAccount,
+} from "@casperdash/usewallet";
+import ConnectWallet from "./components/ConnectWallet";
 import BoardView from "./components/Board";
 import "./main.scss";
 import "./styles.scss";
 
+const client = createClient({
+  connectors: [new CasperSignerConnector(), new CasperDashConnector()],
+  autoConnect: true,
+});
+
 const App = () => {
-  return <BoardView />;
+  const { publicKey } = useAccount();
+  return (
+    <CasperProvider client={client}>
+      {publicKey ? <BoardView /> : <ConnectWallet />}
+    </CasperProvider>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
